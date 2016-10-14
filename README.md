669e9136f0623bff0d09ded9fe6ac7e4ad002a35
Sammy Lin <sammylintw@gmail.com>
2016-10-14 21:37:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# upcase_service
+
+## Installing project
+
+```
+$ python setup.py install
+```
+
+## Running project
+```
+$ python -m upcase_service
+```
+
+## Hacking
+
+To run tests:
+```
+$ python setup.py test
+```
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+@app.route('/<word>')
+def convert_to_uppercase(word):
+    return word.upper()
+
+
+if __name__ == "__main__":
+    app.run()
